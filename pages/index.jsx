8294929803b718c9a757e9fb39b1ce6ad4ee3b91
fassiliss <fassiliss@gmail.com>
2025-10-29 13:03:07 -0500

    Clean up portfolio structure

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,14 @@
 import About from "@/src/components/About";
-// import Blog from "@/src/components/Blog";
-import Client from "@/src/components/Client";
 import Contact from "@/src/components/Contact";
 import Home from "@/src/components/Home";
-// import BlogPopup from "@/src/components/popup/BlogPopup";
 import ImageView from "@/src/components/popup/ImageView";
 import PortfolioPopup from "@/src/components/popup/PortfolioPopup";
 import VideoPopup from "@/src/components/popup/VideoPopup";
 import Services from "@/src/components/Services";
-import Testimonials from "@/src/components/Testimonials";
 import { AxoContext } from "@/src/Context";
 import Cursor from "@/src/layouts/Cursor";
 import Footer from "@/src/layouts/Footer";
 import Header from "@/src/layouts/Header";
-import PreLoader from "@/src/layouts/PreLoader";
 import ScrollTop from "@/src/layouts/ScrollTop";
 import { axo } from "@/src/utils";
 import dynamic from "next/dynamic";
@@ -30,7 +25,7 @@ const Index = () => {
     axo.movingAnimation();
     axo.animation();
   }, []);
-  const { blog, portfolio_modal } = useContext(AxoContext);
+  const { portfolio_modal } = useContext(AxoContext);
   return (
     <Fragment>
       {/* <PreLoader /> */}
@@ -42,8 +37,6 @@ const Index = () => {
       <Services />
       <About />
       <Portfolio />
-      <Testimonials />
-      <Client />
       <Contact />
       <Footer />
       <ScrollTop />
