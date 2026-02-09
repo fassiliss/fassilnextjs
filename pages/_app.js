fe30cd8cf466f8c7e9bf2cd301e1af7962b58b63
fassiliss <fassiliss@gmail.com>
2026-02-08 19:30:56 -0600

    Remove @next/font and update gitignore

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import AxoState from "@/src/Context";
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <AxoState>
-      <Component {...pageProps} />
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
     </AxoState>
   );
 }
